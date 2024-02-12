31689f68d7d0071ec77e64218c8f5b8de28951dd
I. van Gamo <vangamo.beta@gmail.com>
2024-02-12 17:56:06 +0100

    Refactor: Divide pages code into different clases.

--- pycurl_crawler/src/main.py
+++ pycurl_crawler/src/main.py
@@ -3,19 +3,21 @@
 
 from pagesExplorer import PagesExplorer
 from wpcare.pages import Pages
+import sys
 
 
 if __name__ == "__main__":
-  url = 'https://python.org/'
+  url = sys.argv[1] if len(sys.argv) > 1 else 'https://python.org/'
+  (protocol, void, domain, *path) = url
 
   explorer = PagesExplorer(url)
-  #explorer.set_limit(1)
+  explorer.set_limit(10)
   explorer.visit()
 
-  p = Pages.get(url='https://www.lupescoto.com/')
+  p = Pages.get(url=url)
 
   print(vars(p))
 
-  ps = Pages.list(site='www.lupescoto.com')
+  ps = Pages.list(site=domain)
 
-  list(print(p.id, p.site, p.url) for p in ps)
+  list(print(p.id, p.site, p.url, p.types) for p in ps)
